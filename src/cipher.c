c0f28e93019dc5820b91c240febeb89b91daeef1
nurfianqodar <77nurfianqodar@gmail.com>
2026-05-05 00:22:48 +0700

    refactor cipher

--- src/cipher.c
+++ src/cipher.c
@@ -2,6 +2,7 @@
 #include "define.h"
 #include "error.h"
 #include "header.h"
+#include <assert.h>
 #include <sodium.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -11,6 +12,10 @@
 static void derive_nonce(uint8_t *out, const uint8_t *bnonce,
 			 const uint8_t *pnonce)
 {
+	assert(out != NULL);
+	assert(bnonce != NULL);
+	assert(pnonce != NULL);
+
 	memcpy(out, bnonce, SEAL_BNONCE_LEN);
 	memcpy(out + SEAL_BNONCE_LEN, pnonce, SEAL_PNONCE_LEN);
 }
@@ -21,10 +26,10 @@ seal_error seal_cipher_init(struct seal_cipher *cipher,
 {
 	if (0 != crypto_pwhash(cipher->key, SEAL_KEY_LEN, (const char *)pwd,
 			       pwd_len, header->salt, SEAL_OPSLIMIT,
-			       SEAL_MEMLIMIT, SEAL_KEYDRV_ALG)) {
-		seal_error_set_msg("key derivation error");
-		return SEAL_E_KEYDRV;
-	}
+			       SEAL_MEMLIMIT, SEAL_KEYDRV_ALG))
+		return seal_error_set_msg("key derivation error"),
+		       SEAL_E_KEYDRV;
+
 	memcpy(cipher->bnonce, header->bnonce, SEAL_BNONCE_LEN);
 	return SEAL_OK;
 }
@@ -34,36 +39,25 @@ seal_error seal_cipher_encrypt(const struct seal_cipher *cipher,
 			       const size_t len, uint8_t *tag_out,
 			       const uint8_t *pnonce)
 {
-	if (!cipher) {
-		seal_error_set_msg("cipher cannot null");
-		return SEAL_E_INVAL;
-	}
-	if (!in) {
-		seal_error_set_msg("plaintext cannot null");
-		return SEAL_E_INVAL;
-	}
-	if (!out) {
-		seal_error_set_msg("ciphertext buffer cannot null");
-		return SEAL_E_INVAL;
-	}
-	if (!tag_out) {
-		seal_error_set_msg("tag buffer cannot null");
-		return SEAL_E_INVAL;
-	}
-	if (!pnonce) {
-		seal_error_set_msg("postfix nonce cannot null");
-		return SEAL_E_INVAL;
-	}
+	if (!cipher)
+		return seal_error_set_msg("cipher cannot null"), SEAL_E_INVAL;
+	if (!in)
+		return seal_error_set_msg("in cannot null"), SEAL_E_INVAL;
+	if (!out)
+		return seal_error_set_msg("out cannot null"), SEAL_E_INVAL;
+	if (!tag_out)
+		return seal_error_set_msg("tag_out cannot null"), SEAL_E_INVAL;
+	if (!pnonce)
+		return seal_error_set_msg("pnonce cannot null"), SEAL_E_INVAL;
 
 	uint8_t nonce[SEAL_NONCE_LEN];
 	derive_nonce(nonce, cipher->bnonce, pnonce);
 
 	if (0 != crypto_aead_aes256gcm_encrypt_detached(
 			 out, tag_out, NULL, in, len, pnonce, SEAL_PNONCE_LEN,
-			 NULL, nonce, cipher->key)) {
-		seal_error_set_msg("encrypt failed");
-		return SEAL_E_ENCRYPT;
-	}
+			 NULL, nonce, cipher->key))
+		return seal_error_set_msg("encrypt failed"), SEAL_E_ENCRYPT;
+
 	return SEAL_OK;
 }
 
@@ -72,36 +66,24 @@ seal_error seal_cipher_decrypt(const struct seal_cipher *cipher,
 			       uint8_t *out, const size_t len,
 			       const uint8_t *pnonce)
 {
-	if (!cipher) {
-		seal_error_set_msg("cipher cannot null");
-		return SEAL_E_INVAL;
-	}
-	if (!in) {
-		seal_error_set_msg("ciphertext cannot null");
-		return SEAL_E_INVAL;
-	}
-	if (!tag) {
-		seal_error_set_msg("tag cannot null");
-		return SEAL_E_INVAL;
-	}
-	if (!out) {
-		seal_error_set_msg("plaintext buffer cannot null");
-		return SEAL_E_INVAL;
-	}
-	if (!pnonce) {
-		seal_error_set_msg("postfix nonce cannot null");
-		return SEAL_E_INVAL;
-	}
+	if (!cipher)
+		return seal_error_set_msg("cipher cannot null"), SEAL_E_INVAL;
+	if (!in)
+		return seal_error_set_msg("in cannot null"), SEAL_E_INVAL;
+	if (!tag)
+		return seal_error_set_msg("tag cannot null"), SEAL_E_INVAL;
+	if (!out)
+		return seal_error_set_msg("out cannot null"), SEAL_E_INVAL;
+	if (!pnonce)
+		return seal_error_set_msg("pnonce cannot null"), SEAL_E_INVAL;
 
 	uint8_t nonce[SEAL_NONCE_LEN];
 	derive_nonce(nonce, cipher->bnonce, pnonce);
 
-	int ret_sodium;
-	if (0 != (ret_sodium = crypto_aead_aes256gcm_decrypt_detached(
-			  out, NULL, in, len, tag, pnonce, SEAL_PNONCE_LEN,
-			  nonce, cipher->key))) {
-		seal_error_set_msg("decrypt failed");
-		return SEAL_E_DECRYPT;
-	}
+	if (0 != crypto_aead_aes256gcm_decrypt_detached(out, NULL, in, len, tag,
+							pnonce, SEAL_PNONCE_LEN,
+							nonce, cipher->key))
+		return seal_error_set_msg("decrypt failed"), SEAL_E_DECRYPT;
+
 	return SEAL_OK;
 }
